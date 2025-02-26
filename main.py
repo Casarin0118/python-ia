04e001ddda9cf486498c9cb9f4d5c64da27e9e90
Casarin0118 <101209015+Casarin0118@users.noreply.github.com>
2025-02-26 17:50:29 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ import os
 
 load_dotenv()
 
-app = Flask(__name__)
+# Configurar API antes de instanciar o modelo
+genai.configure(api_key=os.getenv('API'))
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/search')
 def search():
     model = genai.GenerativeModel('gemini-1.0-pro-latest')
-    genai.configure(api_key=os.getenv('API'))
-    context = 'Responda como se fosse uma inteligencia artificial chamada Pedro'
+    context = 'Responda como se fosse uma inteligência artificial chamada Pedro'
     prompt = request.args.get('prompt')
+    
+    if not prompt:
+        return {'error': 'Nenhum prompt fornecido'}, 400  # Retorna erro 400 se não houver prompt
+
     input_ia = f'{context} : {prompt}'
     output = model.generate_content(input_ia)
+    
     return {'message': output.text}
-
 
 if __name__ == '__main__':
     app.run(debug=True)
